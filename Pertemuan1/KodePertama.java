f3cdcd90192c7e3c5600bcbd111e253b9549a6d2
ZalikhahKhai <chachakhairunnisa21@gmail.com>
2026-02-12 14:32:00 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,13 @@
+    /*
+Nama: Zalikhah Khairunnisa
+NIM: D1041241018
+Tanggal: 05/02/2026  
+*/
+package Pertemuan1;
+
+public class KodePertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+}
---
+++ Pertemuan1/KodePertama_Latihan.java
@@ -0,0 +1,15 @@
+/*
+Nama: Zalikhah Khairunnisa
+NIM: D1041241018
+Tanggal: 05/02/2026  
+*/
+package Pertemuan1;
+
+public class KodePertama_Latihan {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Zalikhah Khairunnisa");
+        System.out.println("NIM     : D1041241018");
+        System.out.println("Semester: 4");
+        System.out.println("Prodi   : Informatika");
+    }
+}
